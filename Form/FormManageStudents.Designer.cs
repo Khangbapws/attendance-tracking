dd0d2a1a22b3da3ad06904815de6b8374ddbb0b6
Khangbapws <khangngo09292004@gmail.com>
2026-01-06 00:03:42 +0200

    complete FormMangageStudents

--- Form/FormManageStudents.Designer.cs
+++ Form/FormManageStudents.Designer.cs
@@ -28,12 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvStudents = new System.Windows.Forms.DataGridView();
+            this.txtStudentId = new System.Windows.Forms.TextBox();
+            this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.txtClass = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvStudents
+            // 
+            this.dgvStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudents.Location = new System.Drawing.Point(12, 208);
+            this.dgvStudents.Name = "dgvStudents";
+            this.dgvStudents.RowHeadersWidth = 102;
+            this.dgvStudents.RowTemplate.Height = 40;
+            this.dgvStudents.Size = new System.Drawing.Size(1587, 692);
+            this.dgvStudents.TabIndex = 0;
+            this.dgvStudents.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvStudents_CellContentClick);
+            // 
+            // txtStudentId
+            // 
+            this.txtStudentId.Location = new System.Drawing.Point(12, 12);
+            this.txtStudentId.Name = "txtStudentId";
+            this.txtStudentId.Size = new System.Drawing.Size(241, 38);
+            this.txtStudentId.TabIndex = 1;
+            this.txtStudentId.Text = "ID";
+            // 
+            // txtStudentName
+            // 
+            this.txtStudentName.Location = new System.Drawing.Point(388, 12);
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new System.Drawing.Size(241, 38);
+            this.txtStudentName.TabIndex = 2;
+            this.txtStudentName.Text = "Name";
+            // 
+            // txtClass
+            // 
+            this.txtClass.Location = new System.Drawing.Point(760, 12);
+            this.txtClass.Name = "txtClass";
+            this.txtClass.Size = new System.Drawing.Size(241, 38);
+            this.txtClass.TabIndex = 3;
+            this.txtClass.Text = "Class";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 83);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(241, 62);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(388, 83);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(241, 62);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(760, 83);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(241, 62);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(12, 928);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(241, 62);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // FormManageStudents
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1611, 1032);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtClass);
+            this.Controls.Add(this.txtStudentName);
+            this.Controls.Add(this.txtStudentId);
+            this.Controls.Add(this.dgvStudents);
+            this.Name = "FormManageStudents";
             this.Text = "Form6";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvStudents;
+        private System.Windows.Forms.TextBox txtStudentId;
+        private System.Windows.Forms.TextBox txtStudentName;
+        private System.Windows.Forms.TextBox txtClass;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClose;
     }
 }
--- Form/FormManageStudents.cs
+++ Form/FormManageStudents.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,108 @@ namespace l11_danh_mục_điện_tử_để_chấm_điểm_học_sinh_14_12_2025
 {
     public partial class FormManageStudents : Form
     {
+        private string filePath = "students.txt";
         public FormManageStudents()
         {
             InitializeComponent();
+            LoadStudents();
+        }
+
+        private void LoadStudents()
+        {
+            dgvStudents.Columns.Clear();
+            dgvStudents.Rows.Clear();
+
+            dgvStudents.Columns.Add("StudentId", "Student ID");
+            dgvStudents.Columns.Add("StudentName", "Student Name");
+            dgvStudents.Columns.Add("Class", "Class");
+
+            if (!File.Exists(filePath))
+                return;
+
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split('|');
+                if (parts.Length == 3)
+                {
+                    dgvStudents.Rows.Add(parts[0], parts[1], parts[2]);
+                }
+            }
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtStudentId.Text == "" || txtStudentName.Text == "" || txtClass.Text == "")
+            {
+                MessageBox.Show("Please fill all fields.");
+                return;
+            }
+
+            string record = txtStudentId.Text + "|" +
+                            txtStudentName.Text + "|" +
+                            txtClass.Text;
+
+            File.AppendAllText(filePath, record + Environment.NewLine);
+
+            LoadStudents();
+            ClearInputs();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (dgvStudents.CurrentRow == null)
+                return;
+
+            int index = dgvStudents.CurrentRow.Index;
+            string[] lines = File.ReadAllLines(filePath);
+
+            lines[index] = txtStudentId.Text + "|" +
+                           txtStudentName.Text + "|" +
+                           txtClass.Text;
+
+            File.WriteAllLines(filePath, lines);
+
+            LoadStudents();
+            ClearInputs();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dgvStudents.CurrentRow == null)
+                return;
+
+            int index = dgvStudents.CurrentRow.Index;
+            var lines = new System.Collections.Generic.List<string>(File.ReadAllLines(filePath));
+            lines.RemoveAt(index);
+
+            File.WriteAllLines(filePath, lines.ToArray());
+
+            LoadStudents();
+            ClearInputs();
+        }
+
+       
+        private void dgvStudents_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                txtStudentId.Text = dgvStudents.Rows[e.RowIndex].Cells[0].Value.ToString();
+                txtStudentName.Text = dgvStudents.Rows[e.RowIndex].Cells[1].Value.ToString();
+                txtClass.Text = dgvStudents.Rows[e.RowIndex].Cells[2].Value.ToString();
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void ClearInputs()
+        {
+            txtStudentId.Clear();
+            txtStudentName.Clear();
+            txtClass.Clear();
         }
     }
 }
